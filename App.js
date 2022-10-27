e3b12ae57d883c14491b658b3d1218498e543fef
sanghee Shin <113161106+shinsanghee@users.noreply.github.com>
2022-10-27 12:32:59 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import ImportConponent from './R003_ImportConponent'
 
 function App() {
     return (
         <div>
             <h1>Start React 200!</h1>
             <p>CSS 적용하기</p>
-        </div>
+            <ImportConponent></ImportConponent>         
+       </div>
     );
 }
 
